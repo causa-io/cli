dc3092fc384fb1613b6398c4881e2ea8b777c10b
Flo <florent.vouin@gmail.com>
2023-05-04 17:46:54 +0200

    ✨ Implement runCli

---
+++ src/command.spec.ts
@@ -0,0 +1,89 @@
+import { mkdtemp, rm } from 'fs/promises';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { runCli } from './command.js';
+import { outputObject } from './command.module.test.js';
+import { writeConfiguration } from './utils.test.js';
+
+describe('command', () => {
+  describe('runCli', () => {
+    let tmpDir: string;
+    let initialExitCode: number | undefined;
+    let initialArgs: string[];
+    let initialCwd: string;
+
+    beforeEach(async () => {
+      initialExitCode = process.exitCode;
+      initialArgs = process.argv;
+      initialCwd = process.cwd();
+      outputObject.workspace = undefined;
+
+      tmpDir = resolve(await mkdtemp('causa-tests-'));
+      await writeConfiguration(tmpDir, 'causa.yaml', {
+        workspace: { name: 'my-workspace' },
+        environments: { dev: { name: '🪛' } },
+        causa: {
+          modules: [
+            fileURLToPath(new URL('./command.module.test.ts', import.meta.url)),
+          ],
+        },
+      });
+    });
+
+    afterEach(async () => {
+      await rm(tmpDir, { recursive: true, force: true });
+      process.argv = initialArgs;
+    });
+
+    it('should return an error when the directory is not a workspace', async () => {
+      process.argv = initialArgs.slice(0, 2);
+
+      await runCli();
+
+      expect(process.exitCode).toEqual(1);
+      process.exitCode = initialExitCode;
+    });
+
+    it('should default to the current working directory', async () => {
+      process.argv = [...initialArgs.slice(0, 2), 'myFunction'];
+      process.chdir(tmpDir);
+
+      await runCli();
+
+      expect(process.exitCode).toEqual(initialExitCode);
+      expect(outputObject.workspace?.rootPath).toEqual(tmpDir);
+      expect(outputObject.workspace?.projectPath).toBeNull();
+      expect(outputObject.workspace?.environment).toBeNull();
+      process.chdir(initialCwd);
+    });
+
+    it('should select the correct environment', async () => {
+      process.argv = [
+        ...initialArgs.slice(0, 2),
+        '-w',
+        tmpDir,
+        'myFunction',
+        '-e',
+        'dev',
+      ];
+
+      await runCli();
+
+      expect(process.exitCode).toEqual(initialExitCode);
+      expect(outputObject.workspace?.rootPath).toEqual(tmpDir);
+      expect(outputObject.workspace?.projectPath).toBeNull();
+      expect(outputObject.workspace?.environment).toEqual('dev');
+    });
+
+    it('should select the correct working directory', async () => {
+      process.argv = [...initialArgs.slice(0, 2), '-w', tmpDir, 'myFunction'];
+
+      await runCli();
+
+      expect(process.exitCode).toEqual(initialExitCode);
+      expect(outputObject.workspace?.rootPath).toEqual(tmpDir);
+      expect(outputObject.workspace?.projectPath).toBeNull();
+      expect(outputObject.workspace?.environment).toBeNull();
+    });
+  });
+});
---
+++ src/command.ts
@@ -0,0 +1,96 @@
+import { WorkspaceContext } from '@causa/workspace';
+import { Command } from 'commander';
+import { CliContext } from './context.js';
+import { createLogger } from './logger.js';
+
+/**
+ * The name of the base command.
+ */
+const COMMAND_NAME = 'cs';
+
+/**
+ * CLI options that can be specified no matter the command.
+ */
+export type GlobalCliOptions = {
+  /**
+   * The working directory for the command.
+   * Defaults to the current directory.
+   */
+  workingDirectory?: string;
+
+  /**
+   * Whether debug logs should be displayed.
+   * Defaults to `false`.
+   */
+  verbose?: boolean;
+
+  /**
+   * The environment to select before running the command.
+   * This may change the configuration to include environment-specific settings.
+   */
+  environment?: string;
+};
+
+/**
+ * Creates the base command with global options.
+ *
+ * @returns The `commander` {@link Command}.
+ */
+function createBaseCommand(): Command {
+  return new Command()
+    .name(COMMAND_NAME)
+    .option(
+      '-w, --workingDirectory <workingDirectory>',
+      'Sets the working directory for the command.',
+    )
+    .option('-v, --verbose', 'Enables debug logs.')
+    .option(
+      '-e, --environment <environment>',
+      'Sets the environment for the command.',
+    );
+}
+
+/**
+ * Parses the global CLI options by running a dummy `commander` command, only configured with global options.
+ *
+ * @returns The parsed options.
+ */
+function parseGlobalOptions(): GlobalCliOptions {
+  return createBaseCommand()
+    .allowUnknownOption() // Command-specific options should be ignored.
+    .helpOption(false) // The `--help` option shouldn't be caught.
+    .exitOverride(() => {}) // The process should not be exited.
+    .parse()
+    .opts();
+}
+
+/**
+ * Runs the CLI, by parsing global options and initializing a {@link CliContext} in the provided working directory.
+ * The {@link WorkspaceContext} will be used to load modules according to the configuration, which will register
+ * available commands.
+ */
+export async function runCli(): Promise<void> {
+  const options = parseGlobalOptions();
+
+  const logger = createLogger({ verbose: options.verbose });
+
+  const program = createBaseCommand().allowExcessArguments(false);
+
+  let context: CliContext;
+  try {
+    const workspaceContext = await WorkspaceContext.init({
+      workingDirectory: options.workingDirectory,
+      environment: options.environment,
+      logger,
+    });
+    context = new CliContext(program, workspaceContext);
+  } catch (error: any) {
+    const message = error.message ?? error;
+    logger.error(`❌ ${message}`);
+    program.outputHelp();
+    process.exitCode = 1;
+    return;
+  }
+
+  await context.program.parseAsync();
+}
