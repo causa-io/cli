1c477704f918bb45a3292e0512283457718f24a2
Flo <florent.vouin@gmail.com>
2023-05-04 17:21:12 +0200

    ✨ Implement the WorkspaceFunctionCommandsBuilder

---
+++ src/errors.ts
@@ -0,0 +1,26 @@
+import { ParentCliCommandDefinition } from './function-decorators/index.js';
+
+/**
+ * An error thrown when two definitions exists of a parent command with the same name.
+ */
+export class DuplicateParentCommandDefinitionError extends Error {
+  constructor(readonly definition: ParentCliCommandDefinition) {
+    super(
+      `Parent command definition '${definition.name}' already exists in the CLI. This is an issue with the loaded modules.`,
+    );
+  }
+}
+
+/**
+ * An error thrown when an unexpected number of arguments is processed from the commander's action.
+ */
+export class UnexpectedWorkspaceFunctionCommandArgumentsError extends Error {
+  constructor(
+    readonly commandArgs: any[],
+    readonly argumentsMapping: string[],
+  ) {
+    super(
+      `Unexpected number of arguments received from the command ('${commandArgs}') against the mapping '${argumentsMapping}'. This is an internal error.`,
+    );
+  }
+}
