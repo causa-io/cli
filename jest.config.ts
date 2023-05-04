46b5dbbb4876bbbeecb698445ff380e46263466b
Flo <florent.vouin@gmail.com>
2023-05-04 09:41:29 +0200

    🔧 Set up package configuration

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  coverageDirectory: '../coverage',
+  collectCoverageFrom: [
+    '**/*.{js,ts}',
+    '!**/index.ts',
+    '!**/*.spec.ts',
+    '!**/*.test.ts',
+  ],
+  coverageProvider: 'v8',
+  rootDir: 'src',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['jest-extended/all'],
+  testMatch: ['**/*.spec.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['js', 'ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+export default config;
